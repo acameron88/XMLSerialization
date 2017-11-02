2011564400782abba538ef9a007e0022080918a6
Angus Cameron <angus.c29@gmail.com>
2017-11-01 23:15:03 -0700

    Json and Protobuff added

---
+++ JSONSerialization/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
